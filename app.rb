eec5f09375ae3406e2d6d7369bbd1de63678530c
Kat Gifford and Nicole Santamaria <nicole@>
2017-04-26 11:38:11 -0700

    update front end to include dealerships and pushing vehicle objects to dealership object properties

--- app.rb
+++ app.rb
@@ -1,12 +1,39 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require './lib/vehicle'
+require './lib/car_dealership'
 require 'pry'
 
 also_reload('lib/**/*.rb')
 
 get('/') do
   erb(:index)
+end
+
+get('/dealerships/new') do
+  erb(:dealerships_form)
+end
+
+get('/dealerships') do
+  @dealerships = Dealerships.all()
+  erb(:dealerships)
+end
+
+post('/dealerships') do
+  name = params.fetch('name')
+  Dealership.new(name).save()
+  @dealerships = Dealership.all()
+  erb(:success)
+end
+
+get('/vehicles/:id') do
+  @vehicles = Vehicle.find(params.fetch("id").to_i())
+  erb(:singular_vehicle_page)
+end
+
+get('/dealerships/:id') do
+  @dealership = Dealership.find(params.fetch("id").to_i())
+  erb(:dealership_vehicles_form)
 end
 
 get('/vehicles') do
@@ -24,10 +51,7 @@ post('/vehicles') do
   year = params.fetch("year")
   vehicle = Vehicle.new(make, model, year)
   vehicle.save()
+  @dealership = Dealership.find(params.fetch('dealership_id').to_i())
+  @dealership.add_vehicle(@vehicle)
   erb(:success)
-end
-
-get('/vehicles/:id') do
-  @vehicles = Vehicle.find(params.fetch("id").to_i())
-  erb(:singular_vehicle_page)
 end
