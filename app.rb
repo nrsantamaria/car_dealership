9631b481dd60adb5b4a12af48f91c48eae89e7c3
Kat Gifford and Nicole Santamaria <kat@>
2017-04-26 10:06:17 -0700

    add front end to dealership

--- app.rb
+++ app.rb
@@ -1,10 +1,33 @@
 require 'sinatra'
 require 'sinatra/reloader'
-require './lib/car_dealership'
+require './lib/vehicle'
 require 'pry'
 
 also_reload('lib/**/*.rb')
 
 get('/') do
   erb(:index)
+end
+
+get('/vehicles') do
+  @vehicles = Vehicle.all
+  erb(:vehicles_page)
+end
+
+get('/vehicles/new') do
+  erb(:add_vehicle_page)
+end
+
+post('/vehicles') do
+  make = params.fetch("make")
+  model = params.fetch("model")
+  year = params.fetch("year")
+  vehicle = Vehicle.new(make, model, year)
+  vehicle.save()
+  erb(:success)
+end
+
+get('/vehicles/:id') do
+  @vehicles = Vehicle.find(params.fetch("id").to_i())
+  erb(:singular_vehicle_page)
 end
